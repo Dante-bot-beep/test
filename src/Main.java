97702a9ff5a9eb495994e6c4795e466041225c68
Dante-bot-beep <Piotr.Ksiazkiewicz2008@interia.pl>
2024-09-16 12:21:57 +0200

    first

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Podaj liczbę");
+        int number = scanner1.nextInt();
+        System.out.println(number + " to jest liczba odczytana z klawiatury");
+        System.out.printf("%d to jest liczba odczytana z klawaitury\n",number);
+        System.out.printf("Tekst");
+
+    }}
